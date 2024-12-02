daecb98c0ac6092eee43ab457a4168f23cadf0c6
Zeyad Anwar <zeyad.adlan@gmail.com>
2024-12-02 02:52:11 +0200

    Added Tree Class with read xml
    the read xml needs more configurations as it only reads the tags without its values
    [The function reads the tags and add it to the tree]

---
+++ src/Backend/Tree.h
@@ -0,0 +1,27 @@
+#ifndef TREE_H_
+#define TREE_H_
+
+#include <string>
+#include <vector>
+
+class Node {
+private:
+	std::string data;
+	std::vector<Node*> branches;
+public:
+	Node(std::string data);
+	friend class Tree;
+};
+
+class Tree {
+private:
+	Node* root;
+public:
+	Tree();
+	void Read_XML(std::string path);
+	void preorder_traversal(Node* node);
+	Node* get_root();
+};
+
+
+#endif
