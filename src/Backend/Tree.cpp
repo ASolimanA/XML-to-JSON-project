8153f734b22ec6d00c0ede354964ae6afdd4ec30
Zeyad Anwar <zeyad.adlan@gmail.com>
2024-12-02 02:52:11 +0200

    Added Tree Class with read xml
    the read xml needs more configurations as it only reads the tags without its values
    [The function reads the tags and add it to the tree]

---
+++ src/Backend/Tree.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include "Tree.h"
+#include <regex>
+#include <fstream>
+#include <stack>
+
+using namespace std;
+
+Node::Node(string data) {
+	this->data = data;
+}
+
+Node* Tree::get_root() {
+	return root;
+}
+
+void Tree::preorder_traversal(Node* node) {
+	if (node == NULL)
+		return;
+	cout << node->data << endl;
+	for (int i = 0; i < node->branches.size(); i++)
+		preorder_traversal(node->branches[i]);
+}
+
+void Tree::Read_XML(string path) {
+	string text, temp;
+	ifstream myFile(path);
+	regex regexSet("\\<([^>]+)\\>");
+	smatch matches;
+	stack<Node*> st;
+	while (getline(myFile, text)) {
+		string::const_iterator it(text.cbegin());
+		while (regex_search(it, text.cend(), matches, regexSet)) {
+			temp = matches[1];
+			if (temp[0] != '/') {
+				Node* attr = new Node(matches[1]);
+				if (root == NULL) {
+					root = attr;
+				}
+				else if (!st.empty()) {
+					st.top()->branches.push_back(attr);
+				}
+				st.push(attr);
+			}
+			else if (!st.empty()) {
+				if (st.top()->data == temp.substr(1))
+					st.pop();
+			}
+			it = matches.suffix().first;
+		}
+	}
+	myFile.close();
+}
---
+++ src/Backend/Tree.h
@@ -0,0 +1,29 @@
+#ifndef TREE_H_
+#define TREE_H_
+
+#include <string>
+#include <vector>
+
+class Node {
+private:
+	std::string data;
+	std::vector<Node*> branches;
+public:
+	Node(std::string data);
+	friend class Tree;
+};
+
+class Tree {
+private:
+	Node* root;
+public:
+	Tree() {
+		root = NULL;
+	}
+	void Read_XML(std::string path);
+	void preorder_traversal(Node* node);
+	Node* get_root();
+};
+
+
+#endif
