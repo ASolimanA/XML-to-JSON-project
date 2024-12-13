e016c49d29a8e19f1b5b40d597a2e2736243612c
AbdElRahman Soliman <bodi79871@gmail.com>
2024-12-13 13:49:40 +0200

    Added validation.h file

---
+++ src/Backend/validation.h
@@ -0,0 +1,23 @@
+#ifndef VALIDATION_H_
+#define VALIDATION_H_
+
+#include <iostream>
+#include <fstream>
+#include <stack>
+#include <vector>
+#include <array>
+
+using namespace std;
+
+typedef enum {beginning ,leaf, aotag, actag} Phase;
+
+class Validator {
+    public:
+    Validator(const string& filePath);
+    bool filePath_vaild();
+    bool validate ();
+    void write_at_line(const string& newText, int lineNumber);
+    void fix ();
+};
+
+#endif
