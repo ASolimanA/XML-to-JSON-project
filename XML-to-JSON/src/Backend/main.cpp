1987f418ef4bab24550ce0b2c731b7e5edbf1410
Ahmed Hussein <ahmedhusseinelshahat1@gmail.com>
2024-12-15 21:46:33 +0200

    starting the CLI

---
+++ XML-to-JSON/src/Backend/main.cpp
@@ -0,0 +1,33 @@
+//#include "mainwindow.h"
+
+//#include <QApplication>
+//#include "convert.cpp"
+//#include "prettierfunc.cpp"
+//#include "validation.h"
+#include <iostream>
+#include <unistd.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+//    if(argv[0]!= "XML-to-JSON.exe"){
+//        cout<<"Invalid command"<<endl;
+//        return 0;
+//    }
+
+string inputFile, outputFile;
+    if(argv[1] == "format"){
+    }
+    if(argv[1] == "json"){
+
+    }
+    if(argv[1] == "mini"){}
+    if(argv[1] == "compress"){}
+    if(argv[1] == "decompress"){}
+    if(argv[1] == "xml_editor"){}
+//    QApplication a(argc, argv);
+//    MainWindow w;
+//    w.show();
+//    return a.exec();
+}
