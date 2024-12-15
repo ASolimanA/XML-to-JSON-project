b54c05d7fb9b17bc74e20929782350a28b1105ce
Mahmoud-Essam-Noureldin <mahmoudnoir24@gmail.com>
2024-12-15 22:00:41 +0200

    getting options from command line

--- XML-to-JSON/src/Backend/main.cpp
+++ XML-to-JSON/src/Backend/main.cpp
@@ -15,6 +15,21 @@ int main(int argc, char *argv[])
 //        cout<<"Invalid command"<<endl;
 //        return 0;
 //    }
+while(char opt = getopt(argc, argv, "i:o:")) {
+        switch (opt) {
+            case 'i':
+                inputFile = optarg;
+                cout << "Input file: " << optarg << endl;
+                break;
+            case 'o':
+                outputFile = optarg;
+                cout << "Output file: " << optarg << endl;
+                break;
+            default:
+                cout << "Invalid command" << endl;
+                return 0;
+        }
+    }
 
 string inputFile, outputFile;
     if(argv[1] == "format"){
