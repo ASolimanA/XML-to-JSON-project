32a6905e7b2ce0119ea6ffb04ea5ed8ba474f4ad
Zeyad Anwar <zeyad.adlan@gmail.com>
2024-12-29 20:56:41 +0200

    (GUI) Graph Window functions template

--- XML-to-JSON/GUI/graphwindow.cpp
+++ XML-to-JSON/GUI/graphwindow.cpp
@@ -12,3 +12,46 @@ GraphWindow::~GraphWindow()
 {
     delete ui;
 }
+
+
+void GraphWindow::on_search_topic_clicked()
+{
+
+}
+
+
+void GraphWindow::on_search_word_clicked()
+{
+
+}
+
+
+void GraphWindow::on_mutual_followers_clicked()
+{
+
+}
+
+
+void GraphWindow::on_suggest_users_clicked()
+{
+
+}
+
+
+void GraphWindow::on_most_active_clicked()
+{
+
+}
+
+
+void GraphWindow::on_most_influncer_clicked()
+{
+
+}
+
+
+void GraphWindow::on_draw_graph_clicked()
+{
+
+}
+
